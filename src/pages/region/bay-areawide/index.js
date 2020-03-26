97732f0d6944d6e3a33db62b2fde450d2ffad110
James Draper <james.draper@duke.edu>
2020-03-26 16:06:04 -0400

    removed cali stuff

--- src/pages/region/bay-areawide/index.js
+++
@@ -1,6 +0,0 @@
-import { Region } from '../../../components/Region'
-import resourceLinks from './resource-links.yml'
-
-const BayArea = () => <Region region="Bay Area" resourceLinks={resourceLinks} />
-
-export default BayArea
--- src/pages/region/south-bay/index.js
+++
@@ -1,6 +0,0 @@
-import { Region } from '../../../components/Region'
-import resourceLinks from './resource-links.yml'
-
-const SanFrancisco = () => <Region region="Bay Area" resourceLinks={resourceLinks} />
-
-export default SanFrancisco
