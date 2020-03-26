1ffdff3c794e1d9a084d2f25e67cfcfcf7f806cd
James Draper <james.draper@duke.edu>
2020-03-26 15:47:18 -0400

    using cali mutual aid repo

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+export const Footer = () => <footer>
+  <p>
+  <small>We list these networks as a public resource. We cannot verify or vouch for any network or individual offerings. Please exercise all necessary judgement when interacting with community members not previously known to you.</small>
+  <a href="https://github.com/jeffrom/cv19-mutual-aid">Want to contribute?</a>
+  </p>
+  <style jsx>{`
+    footer {
+      width: 100%;
+      height: 100px;
+      border-top: 1px solid #eaeaea;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    footer img {
+      margin-left: 0.5rem;
+    }
+
+    footer a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  `}</style>
+</footer>
---
+++ src/pages/region/bay-areawide/index.js
@@ -0,0 +1,6 @@
+import { Region } from '../../../components/Region'
+import resourceLinks from './resource-links.yml'
+
+const BayArea = () => <Region region="Bay Area" resourceLinks={resourceLinks} />
+
+export default BayArea
